c7b95a315c00df360f6af409ed70ef3ed842789b
Aaron Padilla <aapadill@student.hive.fi>
2025-03-24 14:12:37 +0200

    02

---
+++ ex02/main.cpp
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aapadill <aapadill@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/24 13:12:37 by aapadill          #+#    #+#             */
+/*   Updated: 2025/03/24 13:12:38 by aapadill         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+
+int main(void)
+{
+	std::string string = "HI THIS IS BRAIN";
+	std::string *stringPTR = &string;
+	std::string &stringREF = string;
+
+	std::cout << "Memory address of string: " << &string << std::endl;
+	std::cout << "Memory address of stringPTR: " << &stringPTR << std::endl;
+	std::cout << "Memory address of stringREF: " << &stringREF << std::endl;
+
+	std::cout << "Value of string: " << string << std::endl;
+	std::cout << "Value of stringPTR: " << *stringPTR << std::endl;
+	std::cout << "Value of stringREF: " << stringREF << std::endl;
+		
+	return (0);
+}
